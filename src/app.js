3a18b99f28cc9d1a4eb93e55c59607d41faa2280
spectrewolf8 <salmantariq8385@gmail.com>
2023-08-17 00:58:31 +0500

    worked a few controls on the car body and little bit of physics in the car

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import WebGL from "three/addons/capabilities/WebGL.js";
 import Stats from "three/addons/libs/stats.module.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import * as CANNON from "cannon-es";
 
 console.log("Imports successful!");
 
@@ -18,13 +19,18 @@ const camera = new THREE.PerspectiveCamera(
   1000,
 );
 const controls = new OrbitControls(camera, renderer.domElement);
+const world = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.81, 0),
+});
+const timeStep = 1 / 60;
 
 //other variables
-let concerned_element;
+let carObject, carPhysicsBody;
 
 function init() {
   //setting up scene
   scene.background = new THREE.Color("#1f1f1f");
+  scene.add(new THREE.AxesHelper(100));
 
   //setting up renderer
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -35,7 +41,7 @@ function init() {
 
   //setting up camera and camera's orbital controls
 
-  camera.position.set(-2.75, 5, 50);
+  camera.position.set(-2.75, 5, 10);
 
   controls.target.set(0, 0.5, 0);
   controls.enableDamping = true;
@@ -44,15 +50,15 @@ function init() {
   //   controls.enableRotate = false;
 
   controls.minDistance = 2.5;
-  controls.maxDistance = 6;
+  controls.maxDistance = 100;
   controls.update();
 
-  //   setting gridhelper
-  const size = 100;
-  const divisions = 50;
-  const gridHelper = new THREE.GridHelper(size, divisions);
-  gridHelper.position.y = -1;
-  scene.add(gridHelper);
+  // //   setting gridhelper
+  // const size = 100;
+  // const divisions = 50;
+  // const gridHelper = new THREE.GridHelper(size, divisions);
+  // gridHelper.position.y = 0;
+  // scene.add(gridHelper);
 
   //setting lights
   const light0 = new THREE.AmbientLight("#ffffff", 0.3);
@@ -68,38 +74,28 @@ function init() {
   light1.rotateX = -20;
   light1.rotateY = -30;
   light1.castShadow = true;
+  // scene.add(new THREE.PointLightHelper(light1,,"" ));
   scene.add(light1);
-
-  //   const light2 = new THREE.PointLight("#ffffff", 300, 100);
-  //   light2.position.set(-30, -10, -20);
-  //   light2.lookAt(0, 0, 0);
-  //   light2.rotateX = 180;
-  //   light2.rotateY = 10;
-  //   scene.add(light2);
-
-  //   const light3 = new THREE.PointLight("#ffffff", 300, 100);
-  //   light3.position.set(+30, -10, -20);
-  //   light3.lookAt(0, 0, 0);
-  //   light3.rotateX = 180;
-  //   light3.rotateY = 10;
-  //   scene.add(light3);
 
   //loading 3D model
   const loader = new GLTFLoader();
   loader.load(
-    "../models/car_lowpoly_mod_1.glb",
+    "../models/car_lowpoly_mod_2.glb",
     function (gltf) {
       const car = gltf.scene;
-      concerned_element = car;
       car.scale.set(5, 5, 5);
       car.position.y = 0;
       car.position.x = 0;
+      // car.add(camera);
       scene.add(car);
       console.log(car);
       //   car.getObjectByName("Circle_2").material.color.set("Magenta");
       car.getObjectByName("Circle_2").material.metalness = 0.8;
       car.getObjectByName("Circle_2").material.roughness = 0.3;
-      console.log(concerned_element);
+      car.add(new THREE.AxesHelper(5));
+
+      carObject = car;
+      console.log(carObject);
     },
     undefined,
     function (error) {
@@ -107,25 +103,201 @@ function init() {
     },
   );
 }
+
+//setting worlds, Objects and physics
+//ground plane body and mesh
+const planeGeometry = new THREE.PlaneGeometry(25, 25);
+const planeMesh = new THREE.Mesh(
+  planeGeometry,
+  new THREE.MeshPhongMaterial({ side: THREE.DoubleSide }),
+);
+planeMesh.rotateX(-Math.PI / 2);
+planeMesh.position.set(0, 0, 0);
+planeMesh.receiveShadow = true;
+scene.add(planeMesh);
+
+const planeBody = new CANNON.Body({ mass: 0, shape: new CANNON.Plane() });
+planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
+world.addBody(planeBody);
+planeMesh.position.copy(planeBody.position);
+planeMesh.quaternion.copy(planeBody.quaternion);
+
+//car body, mesh is the model
+// const cubeGemotery = new THREE.BoxGeometry(3, 2, 1);
+// const cubeMesh = new THREE.Mesh(
+//   cubeGemotery,
+//   new THREE.MeshBasicMaterial({
+//     side: THREE.DoubleSide,
+//     wireframe: true,
+//   }),
+// );
+// cubeMesh.add(new THREE.AxesHelper(5));
+// // scene.add(cubeMesh);
+// // carObject = cubeMesh;
+const cubeBody = new CANNON.Body({
+  mass: 1,
+  shape: new CANNON.Box(new CANNON.Vec3(1.5, 1, 0.5)),
+  type: CANNON.Body.DYNAMIC,
+  position: new CANNON.Vec3(0, 20, 0),
+});
+cubeBody.position.y = 0;
+cubeBody.angularFactor = new CANNON.Vec3(0, 1, 0);
+world.addBody(cubeBody);
+carPhysicsBody = cubeBody;
+
+//event handling
+// Toggle pause/play state on "p" key press
+let isPaused = false;
+document.addEventListener("keydown", (event) => {
+  if (event.key === "p") {
+    isPaused = !isPaused;
+    if (!isPaused) {
+      animate(); // Resume animation
+    }
+  }
+});
+
+//car controls
+const keyState = {};
+
+document.addEventListener("keydown", (event) => {
+  keyState[event.key] = true;
+});
+
+document.addEventListener("keyup", (event) => {
+  keyState[event.key] = false;
+});
+
+//Varibles handling rotations
+// Define a variable to keep track of the current rotation angle
+let lateralRotation = 0;
+
+// Define the maximum rotation angle
+const maxLateralRotation = Math.PI / 4; // 30 degrees in radians
+
+// Define the rotation speed
+const rotationSpeed = 0.15;
+
 // Define the start and end colors for the gradient
 let hue = 0;
 const maxHue = 360;
 const hueStep = 0.3;
 
 function animate() {
-  requestAnimationFrame(animate);
-  controls.update();
-  stats.update();
-  renderer.render(scene, camera);
+  if (!isPaused) {
+    requestAnimationFrame(animate);
+    controls.update();
+    stats.update();
+    renderer.render(scene, camera);
+    world.step(timeStep);
+    //handling default positions
 
-  if (concerned_element) {
-    // Calculate the current color based on a smooth transition
-    const hueNormalized = hue / maxHue;
-    const color = new THREE.Color().setHSL(hueNormalized, 1, 0.5);
+    // //handling key events and physics
+    // if (keyState["w"]) {
+    //   carPhysicsBody.velocity.z += 0.5;
+    // }
+    // if (keyState["a"]) {
+    //   carPhysicsBody.velocity.x += 0.5;
+    //   carPhysicsBody.quaternion.setFromEuler(0, -Math.PI / 2, 0);
+    // }
+    // if (keyState["s"]) {
+    //   carPhysicsBody.velocity.z -= 0.5;
+    // }
+    // if (keyState["d"]) {
+    //   carPhysicsBody.velocity.x -= 0.5;
+    //   carPhysicsBody.quaternion.setFromEuler(0, Math.PI / 2, 0);
+    // }
 
-    concerned_element.getObjectByName("Circle_2").material.color.set(color);
+    //handling key events and physics
+    if (keyState["w"]) {
+      // carPhysicsBody.velocity.z += 0.5;
+      carPhysicsBody.applyLocalForce(
+        new CANNON.Vec3(0, 0, 10),
+        new CANNON.Vec3(0, 0, 0),
+      );
+      console.log("Velocity: " + carPhysicsBody.velocity);
 
-    hue = (hue + hueStep) % maxHue;
+      console.log(
+        carPhysicsBody.force.x,
+        carPhysicsBody.force.y,
+        carPhysicsBody.force.z,
+      );
+    }
+    if (keyState["s"]) {
+      carPhysicsBody.applyLocalForce(
+        new CANNON.Vec3(0, 0, -10),
+        new CANNON.Vec3(0, 0, 0),
+      );
+      console.log(
+        carPhysicsBody.force.x,
+        carPhysicsBody.force.y,
+        carPhysicsBody.force.z,
+      );
+    }
+    if (keyState["a"]) {
+      if (lateralRotation < maxLateralRotation) {
+        lateralRotation += rotationSpeed;
+      }
+      carPhysicsBody.applyLocalForce(
+        new CANNON.Vec3(0, 0, 7),
+        new CANNON.Vec3(0, 0, 0),
+      );
+      console.log(
+        carPhysicsBody.force.x,
+        carPhysicsBody.force.y,
+        carPhysicsBody.force.z,
+      );
+    } else if (keyState["d"]) {
+      if (lateralRotation > -maxLateralRotation) {
+        lateralRotation -= rotationSpeed;
+      }
+      carPhysicsBody.applyLocalForce(
+        new CANNON.Vec3(0, 0, 7),
+        new CANNON.Vec3(0, 0, 0),
+      );
+      console.log(
+        carPhysicsBody.force.x,
+        carPhysicsBody.force.y,
+        carPhysicsBody.force.z,
+      );
+    } else {
+      // If neither A nor D is pressed, gradually return to normal rotation
+      if (lateralRotation > 0) {
+        lateralRotation -= rotationSpeed;
+      } else if (lateralRotation < 0) {
+        lateralRotation += rotationSpeed;
+      }
+    }
+
+    // carPhysicsBody.quaternion.setFromEuler(0, Math.PI + lateralRotation, 0);
+
+    carPhysicsBody.quaternion.setFromEuler(0, lateralRotation, 0);
+
+    //actions to do when models loads
+    if (carObject) {
+      // Calculate and set the current color based on a smooth transition
+      const hueNormalized = hue / maxHue;
+      const color = new THREE.Color().setHSL(hueNormalized, 1, 0.5);
+      carObject.getObjectByName("Circle_2").material.color.set(color);
+      hue = (hue + hueStep) % maxHue;
+
+      //calculate positions
+      carObject.position.copy(carPhysicsBody.position);
+      carObject.quaternion.copy(carPhysicsBody.quaternion);
+      if (camera) {
+        // // //camera movements
+        // camera.position.x = carObject.position.x + 2;
+        // camera.position.y = carObject.position.y + 2;
+        // camera.position.z = carObject.position.z + 2;
+        // // controls.target.set(carObject);
+        // const newObjectPosition = new Vector3();
+        // object.getWorldPosition(newObjectPosition);
+        // const delta = newObjectPosition.clone().sub(carObject.position);
+        // camera.position.add(delta);
+      }
+
+      // console.log(carObject.position);
+    }
   }
 }
 
